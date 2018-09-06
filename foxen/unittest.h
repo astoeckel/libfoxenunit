199b6d50fbc52168e67b004d361199a260266b81
Andreas Stöckel <astoecke@uwaterloo.ca>
2018-09-05 23:23:00 -0400

    Better error messages and more colours

--- foxen/unittest.h
+++ foxen/unittest.h
@@ -81,6 +81,7 @@ static void fx_print(char const *s) {
 #define FX_ANSI_RED "\33[38;5;166;1m"
 #define FX_ANSI_GRAY "\33[37;2m"
 #define FX_ANSI_GREEN "\33[38;5;40;1m"
+#define FX_ANSI_ORANGE "\33[38;5;220;1m"
 #define FX_ANSI_RESET "\33[0m"
 
 #define FX_ABS(X) ((X) >= 0 ? (X) : (-(X)))
@@ -88,75 +89,83 @@ static void fx_print(char const *s) {
 #define FX_STR_DETAIL(X) #X
 #define FX_STR(X) FX_STR_DETAIL(X)
 
-#define EXPECT(should, is, rel)                                    \
-	do {                                                           \
-		fx_this_test_failed = 0;                                   \
-		if (!((should)rel(is))) {                                  \
-			fx_print(FX_ANSI_RED "[ERR]" FX_ANSI_RESET             \
-			                     " Assertion failed in " __FILE__  \
-			                     ", line " FX_STR(__LINE__) "\n"); \
-			fx_test_failed = 1;                                    \
-			fx_this_test_failed = 1;                               \
-		}                                                          \
+#define FX_TO_BOOL(X) (((X) != 0) ? 1 : 0)
+
+#define FX_PRINT_ERR(STR) fx_print(FX_ANSI_RED "[ERR]" FX_ANSI_RESET " " STR)
+#define FX_PRINT_WARN(STR) \
+	fx_print(FX_ANSI_ORANGE "[!!!]" FX_ANSI_RESET " " STR)
+#define FX_PRINT_OK(STR) fx_print(FX_ANSI_GREEN "[OK!]" FX_ANSI_RESET " " STR)
+#define FX_PRINT_ARROW(STR) fx_print(FX_ANSI_GRAY "---->" FX_ANSI_RESET " " STR)
+#define FX_PRINT_INDENT(STR) fx_print("      " STR)
+
+#define EXPECT(should, is, rel, msg)                       \
+	do {                                                   \
+		fx_this_test_failed = 0;                           \
+		if (!((should)rel(is))) {                          \
+			FX_PRINT_ERR("Assertion failed in " __FILE__   \
+			             ", line " FX_STR(__LINE__) "\n"); \
+			FX_PRINT_WARN("Expected «" msg "»\n");         \
+			fx_test_failed = 1;                            \
+			fx_this_test_failed = 1;                       \
+		}                                                  \
 	} while (0)
 
-#define ASSERT(should, is, rel)                \
+#define ASSERT(should, is, rel, msg)           \
 	do {                                       \
-		EXPECT(should, is, rel);               \
+		EXPECT(should, is, rel, msg);          \
 		if (fx_this_test_failed) {             \
 			longjmp(fx_test_assert_buffer, 1); \
 		}                                      \
 	} while (0)
 
-#define RUN(test)                                                            \
-	do {                                                                     \
-		fx_test_failed = 0;                                                  \
-		fx_print(FX_ANSI_GRAY "---->" FX_ANSI_RESET " Running test \"" #test \
-		                      "\"\n");                                       \
-		if (!setjmp(fx_test_assert_buffer)) {                                \
-			test();                                                          \
-		}                                                                    \
-		if (fx_test_failed) {                                                \
-			fx_test_n_failed++;                                              \
-			fx_print(FX_ANSI_RED "[ERR]" FX_ANSI_RESET " Test \"" #test      \
-			                     "\" failed!\n");                            \
-		} else {                                                             \
-			fx_test_n_success++;                                             \
-			fx_print(FX_ANSI_GREEN "[OK!]" FX_ANSI_RESET " Test \"" #test    \
-			                       "\" successful\n");                       \
-		}                                                                    \
+#define RUN(test)                                           \
+	do {                                                    \
+		fx_test_failed = 0;                                 \
+		FX_PRINT_ARROW("Running test \"" #test "\"\n");     \
+		if (!setjmp(fx_test_assert_buffer)) {               \
+			test();                                         \
+		}                                                   \
+		if (fx_test_failed) {                               \
+			fx_test_n_failed++;                             \
+			FX_PRINT_ERR("Test \"" #test "\" failed!\n");   \
+		} else {                                            \
+			fx_test_n_success++;                            \
+			FX_PRINT_OK("Test \"" #test "\" successful\n"); \
+		}                                                   \
 	} while (0)
 
-#define DONE                                                         \
-	do {                                                             \
-		fx_print(FX_ANSI_GRAY "=====" FX_ANSI_RESET "\n");           \
-		if (fx_test_n_failed) {                                      \
-			fx_print(FX_ANSI_RED "[ERR]" FX_ANSI_RESET);             \
-		} else {                                                     \
-			fx_print(FX_ANSI_GREEN "[OK!]" FX_ANSI_RESET);           \
-		}                                                            \
-		fprintf(stderr, " Successful tests: %d; Failed tests: %d\n", \
-		        fx_test_n_success, fx_test_n_failed);                \
-		return fx_test_n_failed ? 1 : 0;                             \
+#define DONE                                                        \
+	do {                                                            \
+		fx_print(FX_ANSI_GRAY "=====" FX_ANSI_RESET "\n");          \
+		if (fx_test_n_failed) {                                     \
+			FX_PRINT_ERR("");                                       \
+		} else {                                                    \
+			FX_PRINT_OK("");                                        \
+		}                                                           \
+		fprintf(stderr, "Successful tests: %d; Failed tests: %d\n", \
+		        fx_test_n_success, fx_test_n_failed);               \
+		return fx_test_n_failed ? 1 : 0;                            \
 	} while (0)
 
-#define EXPECT_EQ(should, is) EXPECT(should, is, ==)
-#define ASSERT_EQ(should, is) ASSERT(should, is, ==)
-#define EXPECT_GT(should, is) EXPECT(should, is, >)
-#define ASSERT_GT(should, is) ASSERT(should, is, >)
-#define EXPECT_GE(should, is) EXPECT(should, is, >=)
-#define ASSERT_GE(should, is) ASSERT(should, is, >=)
-#define EXPECT_LT(should, is) EXPECT(should, is, <)
-#define ASSERT_LT(should, is) ASSERT(should, is, <)
-#define EXPECT_LE(should, is) EXPECT(should, is, <=)
-#define ASSERT_LE(should, is) ASSERT(should, is, <=)
-#define EXPECT_NE(should, is) EXPECT(should, is, !=)
-#define ASSERT_NE(should, is) ASSERT(should, is, !=)
-#define EXPECT_TRUE(is) EXPECT(1, ((is) != 0) ? 1 : 0, ==)
-#define ASSERT_TRUE(is) ASSERT(1, ((is) != 0) ? 1 : 0, ==)
-#define EXPECT_FALSE(is) EXPECT(0, ((is) != 0) ? 1 : 0, ==)
-#define ASSERT_FALSE(is) ASSERT(0, ((is) != 0) ? 1 : 0, ==)
-#define EXPECT_NEAR(should, is, err) EXPECT_LE(FX_ABS(should - is), err)
-#define ASSERT_NEAR(should, is, err) ASSERT_LE(FX_ABS(should - is), err)
+#define EXPECT_EQ(should, is) EXPECT(should, is, ==, #is " == " #should)
+#define ASSERT_EQ(should, is) ASSERT(should, is, ==, #is " == " #should)
+#define EXPECT_GT(should, is) EXPECT(should, is, >, #should " > " #is)
+#define ASSERT_GT(should, is) ASSERT(should, is, >, #should " > " #is)
+#define EXPECT_GE(should, is) EXPECT(should, is, >=, #should " >= " #is)
+#define ASSERT_GE(should, is) ASSERT(should, is, >=, #should " >= " #is)
+#define EXPECT_LT(should, is) EXPECT(should, is, <, #should " < " #is)
+#define ASSERT_LT(should, is) ASSERT(should, is, <, #should " < " #is)
+#define EXPECT_LE(should, is) EXPECT(should, is, <=, #should " <= " #is)
+#define ASSERT_LE(should, is) ASSERT(should, is, <=, #should " <= " #is)
+#define EXPECT_NE(should, is) EXPECT(should, is, !=, #is " != " #should)
+#define ASSERT_NE(should, is) ASSERT(should, is, !=, #is " != " #should)
+#define EXPECT_TRUE(is) EXPECT(1, FX_TO_BOOL(is), ==, "bool(" #is ") == TRUE")
+#define ASSERT_TRUE(is) ASSERT(1, FX_TO_BOOL(is), ==, "bool(" #is ") == TRUE")
+#define EXPECT_FALSE(is) EXPECT(0, FX_TO_BOOL(is), ==, "bool(" #is ") == FALSE")
+#define ASSERT_FALSE(is) ASSERT(0, FX_TO_BOOL(is), ==, "bool(" #is ") == FALSE")
+#define EXPECT_NEAR(should, is, err) \
+	EXPECT(FX_ABS(should - is), err, <=, "|" #should " - " #is "| <= " #err)
+#define ASSERT_NEAR(should, is, err) \
+	ASSERT(FX_ABS(should - is), err, <=, "|" #should " - " #is "| <= " #err)
 
 #endif /* FOXEN_UNITTEST_H */
